f65ad288889c0881c59aac2715b2c9360356c578
Erin Stansbury <ekstansbury@gmail.com>
2020-11-21 16:51:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do 
     enable :sessions
-    set :session_secret, "lab"
+    set :session_secret, "item"
   end
   
   get '/' do
     erb :index
+  end
+  
+  post '/checkout' do
+    
   end
   
 end
